9b59b8502ffd7452bddd6ac6ac65c968bb353789
g00387856 <60290531+g00387856@users.noreply.github.com>
2020-02-01 09:52:55 +0000

    Add files via upload

---
+++ lab03.01-hello.py
@@ -0,0 +1,7 @@
+#lab03.01-hello.py
+#stringsandnumbers exercise
+#create a program that reads in a persons names and uses the format function 
+#with strings to outout "hello thename"
+
+N = ("michelle moran")
+print ( "hello{}".format (N))
