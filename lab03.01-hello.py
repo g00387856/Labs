15a73e876d41e87e7ce4eb993c9dd8ba6bf5e16c
g00387856 <60290531+g00387856@users.noreply.github.com>
2020-02-01 10:16:17 +0000

    Delete lab03.01-hello.py

--- lab03.01-hello.py
+++
@@ -1,7 +0,0 @@
-#lab03.01-hello.py
-#stringsandnumbers exercise
-#create a program that reads in a persons names and uses the format function 
-#with strings to outout "hello thename"
-
-N = ("michelle moran")
-print ( "hello{}".format (N))
